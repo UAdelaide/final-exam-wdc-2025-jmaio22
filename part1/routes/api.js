2622558818bffb80df251596dc70e8805dca55a4
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 08:50:58 +0000

    [automatic commit] 2025-06-17 08:50:58 UTC 20.227.140.177

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2/promise');
 
-router.get('/dogs', function(req, res, next) {
+router.get('/dogs', async function(req, res, next) {
+    try {
+        db = await mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'DogWalkService',
+        multipleStatements: true
+        });
+    }
 });
 
 router.get('/walkrequests/open', function(req, res, next) {
