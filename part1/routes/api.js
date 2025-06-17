11bb7063e5a412866b1b950c51703ec1ae5aab0b
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 08:54:59 +0000

    [automatic commit] 2025-06-17 08:54:59 UTC 20.227.140.177

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -14,6 +14,7 @@ router.get('/dogs', async function(req, res, next) {
         });
         const [dogs] = await db.query('SELECT * FROM Dogs;');
         await db.end();
+        
     } catch (err) {
     console.error('Error setting up database.', err);
   }
