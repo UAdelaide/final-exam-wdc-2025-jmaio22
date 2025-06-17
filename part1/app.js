c0daca9cb0c7021b7f731491fe75d260d8b0ebb2
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 08:46:17 +0000

    [automatic commit] 2025-06-17 08:46:17 UTC 20.227.140.177

--- part1/app.js
+++ part1/app.js
@@ -48,7 +48,7 @@ let db;
     });
 
     // insert test data into db
-    await db.execute(`
+    await db.query(`
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner');
