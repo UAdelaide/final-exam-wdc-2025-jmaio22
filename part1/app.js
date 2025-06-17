023b45087e7f37b111dbeb6d7ca6176527a371b2
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 11:20:50 +0000

    [automatic commit] 2025-06-17 11:20:50 UTC 20.227.140.176

--- part1/app.js
+++ part1/app.js
@@ -126,7 +126,7 @@ app.get('/api/walkers/summary', async function(req, res, next) {
         const [walkers] = await sqldb.query("SELECT * FROM Users WHERE role = 'walker';");
         await db.end();
         for (let walker of walkers) {
-            
+            console.log(walker);
         }
         res.json(walkers);
     } catch (err) {
