4fcae1f66d4776e8e1c71e75a9b75803bf12b3a5
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 11:38:13 +0000

    [automatic commit] 2025-06-17 11:38:13 UTC 20.227.140.176

--- part1/app.js
+++ part1/app.js
@@ -127,6 +127,7 @@ app.get('/api/walkers/summary', async function(req, res, next) {
         for (let walker of walkers) {
             let [total_ratings] = await sqldb.query(`SELECT COUNT(walker_id) FROM WalkRatings WHERE walker_id = ${walker.user_id};`);
             total_ratings = total_ratings[0]["COUNT(walker_id)"];
+            let [ratings] = await sqldb.query(`SELECT COUNT(walker_id) FROM WalkRatings WHERE walker_id = ${walker.user_id};`);
         }
         await db.end();
         res.json(walkers);
