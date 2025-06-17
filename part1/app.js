1d9f3df67017efb29753bce3eb538d71632da2df
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 13:35:19 +0000

    [automatic commit] 2025-06-17 13:35:19 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -132,7 +132,7 @@ app.get('/api/walkers/summary', async function(req, res, next) {
             promises.push(sqldb.query(`SELECT COUNT(walker_id) AS value FROM WalkRatings WHERE walker_id = ${walker.user_id};`));
             // get average ratings
             promises.push(sqldb.query(`SELECT AVG(rating) AS value FROM WalkRatings WHERE walker_id = ${walker.user_id};`));
-            // gets only walk applications that are marked as accepted, that have paired to them
+            // gets all walk applications that are marked as accepted, that have paired to them
             // walk requests that are marked as complete, both sharing the same user_id
             // this is what i assume is wanted for completed_walks?
             promises.push(sqldb.query(`SELECT COUNT(walker_id) AS value FROM WalkApplications JOIN WalkRequests ON WalkApplications.request_id = WalkRequests.request_id WHERE WalkRequests.status = 'completed' AND WalkApplications.status = 'accepted' AND WalkApplications.walker_id = ${walker.user_id};`));
