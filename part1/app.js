ea748c406853ea6498c19d027c899199130a0ce3
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 13:18:16 +0000

    [automatic commit] 2025-06-17 13:18:16 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -132,7 +132,7 @@ app.get('/api/walkers/summary', async function(req, res, next) {
             let [total_ratings] = await sqldb.query(`SELECT COUNT(walker_id) AS total FROM WalkRatings WHERE walker_id = ${walker.user_id};`);
             total_ratings = total_ratings[0].total;
             // get average ratings
-            let [average_rating] = await sqldb.query(`SELECT AVG(rating) AS avg FROM WalkRatings WHERE walker_id = ${walker.user_id};`);
+            let [average_rating] = sqldb.query(`SELECT AVG(rating) AS avg FROM WalkRatings WHERE walker_id = ${walker.user_id};`);
             average_rating = average_rating[0].avg;
             // gets applications marked as accepted paired to walk requests marked as completed that are under the same user_id
             let [completed_walks] = await sqldb.query(`SELECT COUNT(walker_id) AS completed_walks FROM WalkApplications JOIN WalkRequests ON WalkApplications.request_id = WalkRequests.request_id WHERE WalkRequests.status = 'completed' AND WalkApplications.status = 'accepted' AND WalkApplications.walker_id = ${walker.user_id};`);
