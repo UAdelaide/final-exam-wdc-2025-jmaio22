8955aac450ee7813dbc5524cbf208430dd985043
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 13:35:39 +0000

    [automatic commit] 2025-06-17 13:35:39 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -129,7 +129,9 @@ app.get('/api/walkers/summary', async function(req, res, next) {
         const promises = [];
         for (let walker of walkers) {
             // get total ratings
-            promises.push(sqldb.query(`SELECT COUNT(walker_id) AS value FROM WalkRatings WHERE walker_id = ${walker.user_id};`));
+            promises.push(sqldb.query(`
+SELECT COUNT(walker_id) AS value FROM WalkRatings WHERE walker_id = ${walker.user_id};
+`));
             // get average ratings
             promises.push(sqldb.query(`SELECT AVG(rating) AS value FROM WalkRatings WHERE walker_id = ${walker.user_id};`));
             // gets all walk applications that are marked as accepted, that have paired to them
