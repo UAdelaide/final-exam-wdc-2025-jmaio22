d12e87e5747e1880fc1df396172128e4dec7f6f6
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 07:30:19 +0000

    [automatic commit] 2025-06-17 07:30:19 UTC 20.227.141.208

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -4,7 +4,10 @@ var router = express.Router();
 router.get('/dogs', function(req, res, next) {
 });
 
-router.get('/dogs', function(req, res, next) {
+router.get('/walkrequests/open', function(req, res, next) {
+});
+
+router.get('/walkers/summary', function(req, res, next) {
 });
 
 module.exports = router;
