860e2ba6416bcdc9df32df37948c130e6e0a7efe
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 08:30:54 +0000

    [automatic commit] 2025-06-17 08:30:54 UTC 20.227.140.177

--- part1/app.js
+++ part1/app.js
@@ -42,17 +42,8 @@ let db;
       host: '127.0.0.1',
       user: 'root',
       password: '',
-      database: 'testdb'
+      database: 'DogWalkService'
     });
-
-    // Create a table if it doesn't exist
-    await db.execute(`
-      CREATE TABLE IF NOT EXISTS books (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255),
-        author VARCHAR(255)
-      )
-    `);
 
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
