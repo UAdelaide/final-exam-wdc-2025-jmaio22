89559b4f6a93c11e2c70d9e62dc82247bfc21431
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-17 13:38:20 +0000

    [automatic commit] 2025-06-17 13:38:20 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -151,6 +151,7 @@ SELECT COUNT(walker_id) AS value FROM WalkApplications JOIN WalkRequests ON Walk
         for (let i = 0; i < results.length; i++) {
           results[i] = results[i][0][0].value;
         }
+
         console.log(test2);
         res.json(test2);
     } catch (err) {
