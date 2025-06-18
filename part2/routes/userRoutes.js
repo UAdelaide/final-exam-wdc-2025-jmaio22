b14bb7288fb7dd969965cce81d38a2c680b8754c
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 02:12:51 +0000

    [automatic commit] 2025-06-18 02:12:51 UTC 20.227.141.211

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -44,12 +44,12 @@ router.post('/login', async (req, res) => {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE email = ? AND password_hash = ?
-    `, [email, bcrypt.hash(password, 10)]); // just pass password by itself to not hash for testing
+    `, [email, bcrypt.hash(password, 10)]); // remove bcrypt.hash if u want to test
 
     const [rows2] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE username = ? AND password_hash = ?
-    `, [email, bcrypt.hash(password, 10)]); // just pass password by itself to not hash for testing
+    `, [email, bcrypt.hash(password, 10)]); // remove bcrypt.hash if u want to test
 
     if (rows.length === 0 && rows2.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
