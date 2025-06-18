d801ffbb889c383de0446fef24e2cd7ddde3fa94
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 05:35:48 +0000

    [automatic commit] 2025-06-18 05:35:48 UTC 20.227.141.211

--- part2/app.js
+++ part2/app.js
@@ -25,5 +25,24 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// final question route
+app.get('/api/dogs', async function(req, res, next) {
+    try {
+        let sqldb;
+        sqldb = await mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'DogWalkService',
+        multipleStatements: true
+        });
+        const [dogs] = await sqldb.query('SELECT Dogs.name AS dog_name, size, Users.username AS owner_username FROM Dogs JOIN Users ON Users.user_id = Dogs.owner_id;');
+        await db.end();
+        res.json(dogs);
+    } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 // Export the app instead of listening here
 module.exports = app;
