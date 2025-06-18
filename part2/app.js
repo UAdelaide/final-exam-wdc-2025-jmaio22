45a3e479262a3f48dca0f5a6fa4cc5e1bc3db27e
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 01:12:20 +0000

    [automatic commit] 2025-06-18 01:12:20 UTC 20.227.141.211

--- part2/app.js
+++ part2/app.js
@@ -16,5 +16,9 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(session({
+    
+}))
+
 // Export the app instead of listening here
 module.exports = app;
