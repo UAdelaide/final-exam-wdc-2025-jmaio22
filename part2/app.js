acaddb960b9e9f91cc9919a8e7c796903fc7aeb8
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 01:24:42 +0000

    [automatic commit] 2025-06-18 01:24:42 UTC 20.227.141.211

--- part2/app.js
+++ part2/app.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 
 const app = express();
 const session = require('express-session');
-const bcrypt = require('bcrypt');
-const db = require('../models/db');
 
 // Middleware
 app.use(express.json());
@@ -26,18 +24,6 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24
     }
 }));
-
-app.post('/auth/login', async function(req, res, next) {
-    try {
-        const user = await db.query(`SELECT * FROM Users WHERE username = ${req.body.username};`);
-        if (!user) {
-            res.status(404).json({ error: 'User not found' });
-        }
-        res.json(user);
-    } catch (err) {
-    res.status(500).json({ error: 'Failed to login' });
-  }
-});
 
 // Export the app instead of listening here
 module.exports = app;
