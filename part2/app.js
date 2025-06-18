175893c6bbabc28d2f5181f0bdd249341a94b505
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 01:19:01 +0000

    [automatic commit] 2025-06-18 01:19:01 UTC 20.227.141.211

--- part2/app.js
+++ part2/app.js
@@ -26,7 +26,23 @@ app.use(session({
     }
 }));
 
-app.post
+app.get('/api/dogs', async function(req, res, next) {
+    try {
+        let sqldb;
+        sqldb = await mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'DogWalkService',
+        multipleStatements: true
+        });
+        const [dogs] = await sqldb.query('SELECT Dogs.name AS dog_name, size, Users.username AS owner_username FROM Dogs JOIN Users ON Users.user_id = Dogs.owner_id;');
+        await db.end();
+        res.json(dogs);
+    } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
 
 // Export the app instead of listening here
 module.exports = app;
