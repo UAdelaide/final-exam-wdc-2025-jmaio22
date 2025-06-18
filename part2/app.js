a9f1f33a89e720f40f22114d4bfa8cea968a76e7
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 01:13:40 +0000

    [automatic commit] 2025-06-18 01:13:40 UTC 20.227.141.211

--- part2/app.js
+++ part2/app.js
@@ -21,8 +21,7 @@ app.use(session({
     resave: false,
     saveUninitialised: true,
     cookie {
-        maxAge: 1000 * 60 * 60 * 24,
-        
+        maxAge: 1000 * 60 * 60 * 24
     }
 }));
 
