dd1784118471d4a7efacc3ef5340dc78eaeded86
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 01:22:22 +0000

    [automatic commit] 2025-06-18 01:22:22 UTC 20.227.141.211

--- part2/app.js
+++ part2/app.js
@@ -31,7 +31,7 @@ app.post('/auth/login', async function(req, res, next) {
     try {
         const user = await db.query(`SELECT * FROM Users WHERE username = ${req.body.username};`);
         if (!user) {
-            res.status(500).json({ error: 'User not found' });
+            res.status(404).json({ error: 'User not found' });
         }
     } catch (err) {
     res.status(500).json({ error: 'Failed to login' });
