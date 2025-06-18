9245c781bbf9fc132522371c51e26cd2f3c50f61
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 02:35:56 +0000

    [automatic commit] 2025-06-18 02:35:56 UTC 20.227.141.211

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,7 +36,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login (dummy version)
+// POST login
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   // const password_hash = await bcrypt.hash(password, 10);
