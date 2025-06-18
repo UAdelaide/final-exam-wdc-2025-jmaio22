bba1bf6a5d02a50313f3c3e614f7418e295fec9f
Jadyn Maiolo <jadynmaiolo@gmail.com>
2025-06-18 04:45:39 +0000

    [automatic commit] 2025-06-18 04:45:39 UTC 20.227.141.211

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -86,7 +86,7 @@ router.get('/dogs', async (req, res) => {
     return res.status(401).json({ error: 'Not logged in' });
   }
   const [dogs] = await db.query(`SELECT * FROM Dogs WHERE owner_id = ${req.session.user.user_id};`);
-  return res.json({ logout: 'Successfully logged out' });
+  return res.json(dogs);
 });
 
 module.exports = router;
